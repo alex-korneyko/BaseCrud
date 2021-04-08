70ce1123a2bff60a226991f26c8329fc27cb8eb5
alex <a.korneyko@gmail.com>
2021-04-08 17:43:01 +0200

    Rename test project

--- BaseCrudTest/Startup.cs
+++ BaseCrudTest/Startup.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-namespace BseCrudTest
+namespace BaseCrudTest
 {
     public class Startup
     {
